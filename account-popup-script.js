a3ca3089f7dd0eb32d4cda1b845b91d277bc1e5c
jeongmin lee <j0064423@hanyang.ac.kr>
2024-03-24 17:19:21 +0900

    Add popup for account information

--- account-popup-script.js
+++ account-popup-script.js
@@ -1,5 +1,11 @@
-function popupAccount(id, name, accountInfo) {
-  const $target = document.getElementById(id);
+function popupClose()
+{
+  const $target = document.getElementById("account-popup");
+  $target.style.display = "none";
+}
+
+function popupAccount(name, accountInfo) {
+  const $target = document.getElementsByClassName("Pop-up-wrapper")[0];
   $target.innerHTML = '<div class="Pop-up">\
   <div class="Title">계좌 번호 복사</div>\
   <div class="Content">' +
@@ -7,19 +13,21 @@ function popupAccount(id, name, accountInfo) {
     `<div class="AccountNumber">${accountInfo}</div>` +
   '</div>\
   <div class="ConfirmButton">\
-    <div class="Left">\
-      <div class="Text" style="color: white">클립보드에 복사</div>\
-    </div>\
-    <div class="Right">\
-      <div class="Text" style="color: black">닫기</div>\
-    </div>\
+    <button class="Left">\
+      <div class="Text" style="color: white">복사</div>\
+    </button>' +
+    `<button class="Right" onclick="popupClose()">` +
+    '  <div class="Text" style="color: black">닫기</div>\
+    </button>\
   </div>\
-</div>';
+  </div>';
+  $target.style.display = "flex";
 }
 
 function generateAccountInfoBoard() {
   const name1 = ["신랑", "어머님", "신부", "아버님", "어머님"];
   const name2 = ["홍은기", "유문숙", "이정민", "이재준", "한승혜"];
+  const account = ["하나 28891034580507", "신협 132004647366", "신한 110-456-124617", "신한 110-517-230136", ""];
 
   const $target = document.getElementById("AccountBoard");
   let text = "";
@@ -30,12 +38,12 @@ function generateAccountInfoBoard() {
         <div class="Line">' +
           `<div class="Text">${name1[i]}</div>` +
           `<div class="Text">${name2[i]}</div></div>` +
-          '<div class="Button" style="margin-left: 20px">\
-            <div class="Text">계좌</div>\
-          </div>\
-          <div class="Button">\
+          `<button class="Button" onclick="popupAccount('${name2[i]}', '${account[i]}')" style="margin-left: 20px">` +
+          '  <div class="Text">계좌</div>\
+          </button>\
+          <button class="Button">\
             <div class="Text">카카오페이</div>\
-          </div>\
+          </button>\
       </div>';
 
       if(i == 1) {
