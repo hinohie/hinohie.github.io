a0aff8444b4cf5c6ab5c9d384ea37b49651d10f0
Eunki Hong <h.pichulia@gmail.com>
2024-03-25 23:06:53 +0900

    Add kakao link (prototype)
    Signed-off-by: Eunki Hong <h.pichulia@gmail.com>

--- account-popup-script.js
+++ account-popup-script.js
@@ -4,17 +4,29 @@ function popupClose()
   $target.style.display = "none";
 }
 
-function popupAccount(name, accountInfo) {
+function copyClipboad(title, text)
+{
+  if (navigator?.clipboard) {
+    navigator.clipboard.writeText(text).then(() => {
+      alert(`${title}` + "가 복사되었습니다.");
+    });
+  } else {
+    window.prompt(`${title}` + "를 복사해 주세요.", text);
+  }
+};
+
+function popupAccount(title, name, bank, accountInfo) {
   const $target = document.getElementsByClassName("Pop-up-wrapper")[0];
-  $target.innerHTML = '<div class="Pop-up">\
-  <div class="Title">계좌 번호 복사</div>\
-  <div class="Content">' +
+  $target.innerHTML = '<div class="Pop-up">' +
+  `<div class="Title">${title}</div>` +
+  '<div class="Content">' +
     `<div class="Name">${name}</div>` +
+    `<div class="Name">${bank}</div>` +
     `<div class="AccountNumber">${accountInfo}</div>` +
   '</div>\
-  <div class="ConfirmButton">\
-    <button class="Left">\
-      <div class="Text" style="color: white">복사</div>\
+  <div class="ConfirmButton">' +
+    `<button class="Left"onclick="copyClipboad('${title}', '${accountInfo}')">` +
+    '  <div class="Text" style="color: white">복사</div>\
     </button>' +
     `<button class="Right" onclick="popupClose()">` +
     '  <div class="Text" style="color: black">닫기</div>\
@@ -27,7 +39,9 @@ function popupAccount(name, accountInfo) {
 function generateAccountInfoBoard() {
   const name1 = ["신랑", "어머님", "신부", "아버님", "어머님"];
   const name2 = ["홍은기", "유문숙", "이정민", "이재준", "한승혜"];
-  const account = ["하나 28891034580507", "신협 132004647366", "신한 110-456-124617", "신한 110-517-230136", ""];
+  const bank = ["하나", "신협", "신한", "신한", ""];
+  const account = ["28891034580507", "132004647366", "110-456-124617", "110-517-230136", ""];
+  const kakao = ["https://qr.kakaopay.com/FHPNUIlfM", "", "", "", ""];
 
   const $target = document.getElementById("AccountBoard");
   let text = "";
@@ -38,11 +52,11 @@ function generateAccountInfoBoard() {
         <div class="Line">' +
           `<div class="Text">${name1[i]}</div>` +
           `<div class="Text">${name2[i]}</div></div>` +
-          `<button class="Button" onclick="popupAccount('${name2[i]}', '${account[i]}')" style="margin-left: 20px">` +
+          `<button class="Button" onclick="popupAccount('계좌번호', '${name2[i]}', '${bank[i]}', '${account[i]}')" style="margin-left: 20px">` +
           '  <div class="Text">계좌</div>\
-          </button>\
-          <button class="Button">\
-            <div class="Text">카카오페이</div>\
+          </button>' +
+          `<button class="Button" onclick="popupAccount('카카오페이 링크','${name2[i]}', '', '${kakao[i]}')">` +
+          '  <div class="Text">카카오페이</div>\
           </button>\
       </div>';
 
