a761848c94aecbf12a06e8674e894183b2827fa9
Eunki Hong <h.pichulia@gmail.com>
2024-04-02 22:22:18 +0900

    Add jeongmin and mom's kakao pay link + Change mom's account
    Signed-off-by: Eunki Hong <h.pichulia@gmail.com>

--- scripts/account-popup-script.js
+++ scripts/account-popup-script.js
@@ -39,9 +39,9 @@ function popupAccount(title, name, bank, accountInfo) {
 function generateAccountInfoBoard() {
   const name1 = ["신랑", "어머님", "신부", "아버님", "어머님"];
   const name2 = ["홍은기", "유문숙", "이정민", "이재준", "한승혜"];
-  const bank = ["하나", "신협", "신한", "대구", "우리"];
-  const account = ["28891034580507", "132004647366", "110456124617", "06213044950", "98000930002001"];
-  const kakao = ["https://qr.kakaopay.com/FHPNUIlfM", "", "", "", ""];
+  const bank = ["하나", "국민", "신한", "대구", "우리"];
+  const account = ["28891034580507", "66620101567477", "110456124617", "06213044950", "98000930002001"];
+  const kakao = ["https://qr.kakaopay.com/FHPNUIlfM", "https://qr.kakaopay.com/Ej8G4aAMr", "https://qr.kakaopay.com/FXSwx4jqW", "", ""];
 
   const $target = document.getElementById("AccountBoard");
   let text = "";
