ddc44945135452d9297d9ab93b770094f2b9b23c
Eunki Hong <h.pichulia@gmail.com>
2024-04-02 22:55:57 +0900

    Add MORE MORE images
    Signed-off-by: Eunki Hong <h.pichulia@gmail.com>

--- scripts/more-images-generator.js
+++ scripts/more-images-generator.js
@@ -27,7 +27,6 @@ const MoreIdLists = [
   "192L9xEb5tWbvJj5CBd7J-yrJkYSohIlT",
   "1AIMVUi-V6oYHkBZcdvHf0UqvFeprwHTO",
   "1BuzcQSor4PjBmQb9oUVro-AlFMMqmx_z",
-  /* /// Block this line, for test. Reopen if we prepare complete!
   "1DGEeqKs6MECrt-qVdHs9-j0p8bafLAoP",
   "1DgMA9Az8eY6xT5flU8kYe-uzsCmMfcVj",
   "1DhkoFfiUjLijAYgFeO8jfx8eKnR1grgk",
@@ -85,7 +84,53 @@ const MoreIdLists = [
   "1xPjNT0g_qvPlbWAYYvTJj50-NWuyvwoI",
   "1xsyDUIQwx3aoXaS35mF31QZFCznAcVGb",
   "1zkUFKXeikS1qP5vPz9FUsyWcH8IN1037",
-  */
+  "1--FqPEf51DfckMsmqeNeKQSRuUxG3V5Y",
+  "1-0-ajZ5Le6O4UrovdAgis_qmTLVwqcVH",
+  "1-4Cl4MirWWy8gt34knYVKZthOm2m7Pw4",
+  "1-FsZ32Un9uiNK7d65tlEFEaX4uqKmKhe",
+  "10q1B_MVUqQUq_v9Y_sjqqvpE7soeOeIN",
+  "10sp_bCfKAOXl2ovGImhJmoWwMZMNmHsj",
+  "11KV0YqRQMipinxsSXpD-8xcOLza4Vlgu",
+  "11QElEK-A4Ho4fL81Y01aI6V6GH9wXNT2",
+  "12GXn4WFLhG4KgegowiXMWfsPag1VorcP",
+  "16OhtXBNYqbmIfMwRSJKeZLbRtjySQKr2",
+  "16Ty1hdFwWs7mFD4OLvED6RuQ6Qa_SrO9",
+  "18OBiK_S7YqI6HPyWszG1nqJuMHFFNgvy",
+  "19NRe_gpgTV5IZj6hbW7ghfHHWSVYEfgz",
+  "19tWDGwPP_MgAeRE8R0_zXw312_Lf_-Sy",
+  "1BrujhpJdHZ7-dCltIBlOuci6B314aJiU",
+  "1DEHdWTobNai43tRAnMt6apqLdMiA-q4O",
+  "1FetM-g_MoGi8OeLWgJHhTd-34-sSiAux",
+  "1JzJjG9Y7VTt7_MAhdC-YWGl8AOt7S28r",
+  "1N4npkSH7Xp9EttIC3DE5NYJgcITr0Fe6",
+  "1PTofS9zRprNToKGWMgzSwGviCrlxFE72",
+  "1QkkGGWHsa7Eglbq50IOimhDp3ARWytca",
+  "1R6vMa5bXO3OytO3h_K6ItsEa1i17sPjA",
+  "1TyDO1rc47MekTdwZjsmORg_W3VdYIyLf",
+  "1ZVlidZHDp9EbOseCNh4nA4WVlStcRjHG",
+  "1ZiiX-HvGOlBXJ8n3iqCTiJBOSKFIs4o7",
+  "1aFCOARi-PkaVtozsEcGDnXSZKADZ2Qr6",
+  "1b59F58x3wezhl1rQMJJl87B2wjbe4gJd",
+  "1cLAMDwVZhrpDuPIhyphfvFRdPI_NI7tz",
+  "1e1E7FSumoCLIp--puuf0603ox2CsjIBT",
+  "1ePQhOyRlnohR_RMuPIy_qb88mpFyYvkA",
+  "1frygz5ry_neRYqOzilTWsRaWWG_ofIiJ",
+  "1gaenlb8J_5jSv6ps_zgqC2FvaQJBwOh3",
+  "1h38lGuRslSpdijyPGU_Po8flsweXBsfx",
+  "1hjZ8ZXB1wAt7XSH1dgn3ijVpFrsLu53N",
+  "1jtZjtcjXbbeBUOV_AUqMCaA3lRNd0Ccw",
+  "1kwFXSEHR0LMYhb5C3BFABzs6M3Ox52Tn",
+  "1mZvH6HEK8CwYCX8jw4lP5YItuI3wnoMq",
+  "1o6zeI2eJeMynys9DD-d25zr6llKcSDI7",
+  "1qbaXq-dG54rasXYOlMav0tkj8DQ4rZrC",
+  "1rNJ3UfogZibj96Xqco3ocTsMkCHf3GOC",
+  "1rW3iiQHK-Vz9XNmOAtUU2PjZhGidm0td",
+  "1ukUOfgxpOJmb4b6HNI3OvP8-mzhpceoD",
+  "1vTofYHR6hEuR0mrLUYIIK3ymlWi2AIGg",
+  "1xD6lvmaMWaIUWnv6Qetc5Ln9uukEB_Q8",
+  "1xS0eIpS5SLD6Z1xQ3Pp69PCoQcub-ani",
+  "1ya2pXIin05CRtMTrDOmfyJ3bw3bzOlOR",
+  "1yqeBcY1C_SROY77EgioRZ6TwsJXODKnc",
 ];
 
 function generateMoreImages() {
@@ -101,7 +146,7 @@ function generateMoreImages() {
   text += `<div class="moreImageContainer">`;
   for(let i = 0; i < MoreIdLists.length; ++i)
   {
-    text += `<img src="https://drive.google.com/thumbnail?sz=w300&id=${MoreIdLists[i]}" alt="gallery-image-more-${i+1}" loading="lazy" width="300" onerror="this.src='./resources/image/favicon.ico';" onclick="ModalOpen(this);"/>`;
+    text += `<img src="https://drive.google.com/thumbnail?sz=w300&id=${MoreIdLists[i]}" alt="gallery-image-more-${i+1}" loading="lazy" width="300" style="min-height:100px;" onerror="this.src='./resources/image/favicon.ico';" onclick="ModalOpen(this);"/>`;
   }
   text += "</div>";
   // console.log(text);
@@ -123,9 +168,9 @@ function generateGallerySwiperImages() {
   text += `<div class="swiper-wrapper">`;
   for(let i = 0; i < IdLists.length; ++i)
   {
-    text += `<div class="swiper-slide"><img src="https://drive.google.com/thumbnail?sz=w300&id=${IdLists[i]}" alt="gallery-image-${i+1}" loading="lazy" width="300" onerror="this.src='./resources/image/favicon.ico';" onclick="ModalOpen(this);"/></div>`;
+    text += `<div class="swiper-slide"><img src="https://drive.google.com/thumbnail?sz=w300&id=${IdLists[i]}" alt="gallery-image-${i+1}" loading="lazy" width="300" style="min-height:100px;" onerror="this.src='./resources/image/favicon.ico';" onclick="ModalOpen(this);"/></div>`;
   }
-  text += `<div class="swiper-slide"><div style="display:grid;vertical-align:middle;background-color:#FBFBFB;aspect-ratio:1/1;align-items:center;margin:auto;max-width:50%;"><button id="galleryMoreButton" onclick="GalleryMoreClicked()" style="aspect-ratio:1/1;">더 보기<br/>(예상 데이터 사용량 : 약 120MB)</button></div></div>`;
+  text += `<div class="swiper-slide"><div style="display:grid;vertical-align:middle;background-color:#FBFBFB;aspect-ratio:1/1;align-items:center;margin:auto;max-width:50%;"><button id="galleryMoreButton" onclick="GalleryMoreClicked()" style="aspect-ratio:1/1;">더 보기<br/>(예상 데이터 사용량 : TODO)</button></div></div>`;
   text += "</div>";
   
   text += `<div class="swiper-button-next"></div>`;
